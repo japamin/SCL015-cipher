2502174e7aae0f00f368f63adcb607d30f07a6b3
japamin <yasmin.bishara.h@gmail.com>
2020-11-10 17:47:51 -0300

    proyecto con flexbox y js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
 console.log(cipher);
+
+document.getElementById("PaginaCifrado").hidden=true
+
+
+
+
+  
+document.getElementById("single-img1").addEventListener("click",function() {
+    document.getElementById("CajasDeFotos").hidden=true;
+    document.getElementById("PaginaCifrado").hidden=false;
+    
+})
+
+
+
+
