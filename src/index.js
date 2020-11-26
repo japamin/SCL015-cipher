6f988e659b8f1f0d7c1319d9d961baaed197f711
japamin <yasmin.bishara.h@gmail.com>
2020-11-26 17:42:50 -0300

    proyecto con cifrado

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
 
 document.getElementById("PaginaCifrado").hidden=true
+
+document.getElementById("PaginaDescifrado").hidden=true
 
   
 document.getElementById("single-img1").addEventListener("click",function() {
@@ -11,11 +13,44 @@ document.getElementById("single-img1").addEventListener("click",function() {
     
 })
 
-document.getElementById("PaginaDescifrado").hidden=true
-
 document.getElementById("single-img2").addEventListener("click",function() {
     document.getElementById("CajasDeFotos").hidden=true;
     document.getElementById("PaginaDescifrado").hidden=false;
     
 })
 
+//.toUpperCase()
+
+function cipher() {
+  let inputMensaje = document.getElementById("containerOne").value.toUpperCase()
+  console.log(1, inputMensaje)
+  let offset = parseInt(document.getElementById("offset").value)
+  console.log(2, offset)
+
+let mensaje = ""
+for (let i = 0; i < inputMensaje.length; i++) {
+     let inputMensajeAscii = inputMensaje.charCodeAt(i);
+     let letterNewPosition = (inputMensajeAscii - 65 + offset) % 26 + 65;
+     if (inputMensajeAscii === 32) { letterNewPosition = 32; }
+     let sinceAscii = String.fromCharCode(letterNewPosition);
+
+     mensaje += sinceAscii;
+     //console.log(4, mensaje)
+
+}
+
+//return inputMensaje;
+document.getElementById("containerTwo").innerHTML = mensaje
+
+
+}
+
+//Funcion desifrado
+
+function desifrado(){
+
+
+
+
+
+} 
